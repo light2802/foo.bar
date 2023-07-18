0f3a7b162ab7931bbc803fa096b9443c6be77fb2
Aarya Chaumal <aarya.chaumal@gmail.com>
2023-07-19 00:23:19 +0530

    Add C3 part 2

---
+++ C3-2/solution.py
@@ -0,0 +1,13 @@
+def solution(l):
+    triple = 0
+    pair = [0] * len(l)
+    
+    for i in range(1, len(l) - 1):
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                pair[i] += 1
+    
+    for i in range(2, len(l)):
+        for j in range(1, i):
+            if l[i] % l[j] == 0:
+                triple += pair[j]
